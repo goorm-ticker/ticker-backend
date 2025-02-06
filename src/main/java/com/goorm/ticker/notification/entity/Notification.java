f6b46b9106664b9b54e63260403becf3f7a694c2
tjdgusdk <sha6639@naver.com>
2025-02-06 15:14:48 +0900

    feat: Notification 엔티티 구현

---
+++ src/main/java/com/goorm/ticker/notification/entity/Notification.java
@@ -0,0 +1,47 @@
+package com.goorm.ticker.notification.entity;
+
+import com.goorm.ticker.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "notification")
+@Getter
+@NoArgsConstructor
+public class Notification extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
+    private Long id;
+
+    // 추후에 User 관계 매핑 예정
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "message", nullable = false)
+    private String message;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private NotificationType type;
+
+    @Column(name = "is_read", nullable = false)
+    private boolean isRead;
+
+    public static Notification createNotification(Long userId, String message, NotificationType type) {
+        Notification notification = new Notification();
+        notification.userId = userId;
+        notification.message = message;
+        notification.type = type;
+        notification.isRead = false;
+        return notification;
+    }
+
+    public void markAsRead() {
+        if (!this.isRead) {
+            this.isRead = true;
+        }
+    }
+}
