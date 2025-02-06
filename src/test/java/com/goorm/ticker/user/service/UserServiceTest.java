b9c8de01bd9101c4e1a2ca18d971c044d273a65e
imzero238 <nayoung8142@gmail.com>
2025-02-06 16:01:18 +0900

    fix: @ActiveProfiles("test") 설정
    - actions에서 java.lang.IllegalStateException 발생

--- src/test/java/com/goorm/ticker/user/service/UserServiceTest.java
+++ src/test/java/com/goorm/ticker/user/service/UserServiceTest.java
@@ -12,10 +12,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserServiceTest {
 
 	@Autowired
