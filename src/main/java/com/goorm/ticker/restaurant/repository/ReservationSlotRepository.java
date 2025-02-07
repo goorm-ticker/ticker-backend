d644a127f67bdfd473fe5521217e7f16f9caf293
Hm-source <swo8852@gmail.com>
2025-02-07 15:08:06 +0900

    fix: 쿼리 생성 실패 에러 수정.

--- src/main/java/com/goorm/ticker/restaurant/repository/ReservationSlotRepository.java
+++ src/main/java/com/goorm/ticker/restaurant/repository/ReservationSlotRepository.java
@@ -4,9 +4,13 @@ import java.time.LocalTime;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.goorm.ticker.restaurant.entity.ReservationSlot;
 
 public interface ReservationSlotRepository extends JpaRepository<ReservationSlot, Long> {
-	Optional<ReservationSlot> findBySlotTimeAndRestaurantId(LocalTime slotTime, Long restaurantId);
+	@Query("SELECT r FROM ReservationSlot r WHERE r.slotTime = :slotTime AND r.restaurant.restaurantId = :restaurantId")
+	Optional<ReservationSlot> findBySlotTimeAndRestaurantId(@Param("slotTime") LocalTime slotTime,
+		@Param("restaurantId") Long restaurantId);
 }
