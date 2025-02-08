6975e9bfc40c55399a36503051ade59e9bc66dba
Do-oya <qkrwjdgus1441@naver.com>
2025-02-09 00:51:03 +0900

    feat: 대기열 Service 구현
    - 대기열 등록, 대기열 입장, 대기열 취소, 현재 사용자의 대기 순서

---
+++ src/main/java/com/goorm/ticker/waitlist/service/CompleteWaitingService.java
@@ -0,0 +1,42 @@
+package com.goorm.ticker.waitlist.service;
+
+import com.goorm.ticker.common.exception.CustomException;
+import com.goorm.ticker.common.exception.ErrorCode;
+import com.goorm.ticker.waitlist.entity.Status;
+import com.goorm.ticker.waitlist.entity.WaitList;
+import com.goorm.ticker.waitlist.repository.WaitListRepository;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CompleteWaitingService {
+
+    private final WaitListRepository waitListRepository;
+    private final HttpSession httpSession;
+
+    public void completeWaiting() {
+        // 세션에서 사용자 ID 가져오기
+        Long userId = getSessionUserId();
+
+        // 사용자 대기열의 정보 조회
+        WaitList waitList = findUserWaitingList(userId);
+
+        waitList.updateStatus(Status.ENTERED);
+        waitListRepository.save(waitList);
+    }
+
+    private Long getSessionUserId() {
+        Long userId = (Long) httpSession.getAttribute("user");
+        if (userId == null)
+            throw new CustomException(ErrorCode.SESSION_EXPIRED);
+
+        return userId;
+    }
+
+    private WaitList findUserWaitingList(Long userId) {
+        return waitListRepository.findByUser_IdAndStatus(userId, Status.WAITING)
+                .orElseThrow(() -> new CustomException(ErrorCode.WAITLIST_NOT_FOUND));
+    }
+}
