975658960f779d44a1bfce9b4f84dac1641662e7
tjdgusdk <sha6639@naver.com>
2025-02-11 13:23:55 +0900

    feat: Firebase 설정 파일 환경 변수

--- src/main/java/com/goorm/ticker/common/config/FirebaseConfig.java
+++ src/main/java/com/goorm/ticker/common/config/FirebaseConfig.java
@@ -4,11 +4,13 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.annotation.PostConstruct;
 import java.io.InputStream;
 
 @Configuration
+@Profile("!test")
 public class FirebaseConfig {
 
     @PostConstruct
