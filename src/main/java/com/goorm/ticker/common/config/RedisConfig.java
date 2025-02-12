9c4bd16d90d6a7ef90da268c696caad0e1bc71c9
tjdgusdk <sha6639@naver.com>
2025-02-12 10:51:56 +0900

    build: Redis 설정 추가

--- src/main/java/com/goorm/ticker/common/config/RedisConfig.java
+++ src/main/java/com/goorm/ticker/common/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.goorm.ticker.common.config;
 
 import com.goorm.ticker.notification.subscriber.ReservationStatusSubscriber;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,6 +11,7 @@ import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@ConditionalOnProperty(name = "spring.redis.enabled", havingValue = "true", matchIfMissing = true)
 public class RedisConfig {
 
     @Bean
