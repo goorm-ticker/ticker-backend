65ba49dd943b214d4239ff096871e9fdfcdaf96d
Hm-source <swo8852@gmail.com>
2025-02-13 14:19:45 +0900

    feat: httpSession 확인 추가

--- src/main/java/com/goorm/ticker/reservation/controller/ReservationController.java
+++ src/main/java/com/goorm/ticker/reservation/controller/ReservationController.java
@@ -10,10 +10,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.goorm.ticker.common.exception.CustomException;
+import com.goorm.ticker.common.exception.ErrorCode;
 import com.goorm.ticker.reservation.dto.request.ReservationCreateRequest;
 import com.goorm.ticker.reservation.dto.response.ReservationCreateResponse;
 import com.goorm.ticker.reservation.service.ReservationService;
 
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -25,17 +28,30 @@ public class ReservationController {
 
 	@PostMapping
 	public ResponseEntity<ReservationCreateResponse> createReservation(
-		@Valid @RequestBody ReservationCreateRequest request) {
-		ReservationCreateResponse response = reservationService.reserve(request);
+		@Valid @RequestBody ReservationCreateRequest request,
+		HttpSession httpSession) {
+		Long userId = (Long)httpSession.getAttribute("userId");
+
+		if (userId == null) {
+			throw new CustomException(ErrorCode.SESSION_EXPIRED);
+		}
+
+		ReservationCreateResponse response = reservationService.reserve(request, userId);
 		return ResponseEntity.ok(response);
 	}
 
 	@PatchMapping("/{reservationId}")
 	public ResponseEntity<ReservationCreateResponse> updateReservation(
 		@PathVariable Long reservationId,
-		@RequestBody Map<String, String> updateRequest) {
+		@RequestBody Map<String, String> updateRequest,
+		HttpSession httpSession) {
 		String status = updateRequest.get("status");
-		ReservationCreateResponse response = reservationService.updateReservation(reservationId, status);
+
+		Long userId = (Long)httpSession.getAttribute("userId");
+		if (userId == null) {
+			throw new CustomException(ErrorCode.SESSION_EXPIRED);
+		}
+		ReservationCreateResponse response = reservationService.updateReservation(reservationId, status, userId);
 		return ResponseEntity.ok(response);
 	}
 }
