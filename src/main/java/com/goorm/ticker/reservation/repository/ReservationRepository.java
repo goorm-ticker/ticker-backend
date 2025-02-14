cbfd61f3cffc45c5a64f308027bd5a546f314148
tjdgusdk <sha6639@naver.com>
2025-02-14 11:27:52 +0900

    feat: 예약 자동 취소 쿼리 추가

--- src/main/java/com/goorm/ticker/reservation/repository/ReservationRepository.java
+++ src/main/java/com/goorm/ticker/reservation/repository/ReservationRepository.java
@@ -1,8 +1,19 @@
 package com.goorm.ticker.reservation.repository;
 
+import com.goorm.ticker.reservation.Entity.ReservationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.goorm.ticker.reservation.Entity.Reservation;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    @Query("SELECT r FROM Reservation r WHERE r.reservationDateTime < :threshold AND r.status <> :status")
+    List<Reservation> findByReservationDateTimeBeforeAndStatusNot(
+            @Param("threshold") LocalDateTime threshold,
+            @Param("status") ReservationStatus status
+    );
 }
